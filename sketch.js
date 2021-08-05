1a4954a5c372ca7c00bd03e596f920846065ebe0
DhruvMaheshwari243 <76244578+DhruvMaheshwari243@users.noreply.github.com>
2021-08-05 13:36:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var issImg, spacebgImg,spacecraft1Img,spacecraft2Img,spacecraft3Img,spacecraft4Img;
+var iss, hasDocked = false, spacecraft;
+function preload(){
+issImg = loadImage("images/iss.png");
+spacebgImg = loadImage("images/spacebg.jpg")
+spacecraft1Img = loadImage("images/spacecraft1.png")
+spacecraft2Img = loadImage("images/spacecraft2.png")
+spacecraft3Img = loadImage("images/spacecraft3.png")
+spacecraft4Img = loadImage("images/spacecraft4.png")
+}
+
+function setup() {
+  createCanvas(800,400);
+  iss = createSprite(400, 100, 50, 50);
+  iss.addImage(issImg);
+  iss.scale = 0.5;
+
+  spacecraft = createSprite(400, 150, 50, 50)
+  spacecraft.addImage(spacecraft1Img);
+  spacecraft.scale = 0.1;
+}
+
+function draw() {
+  background(spacebgImg);  
+
+  if(!hasDocked){
+   // spacecraft.x = Math.round(random(200,400));
+   if(keyDown(37)){
+    spacecraft.addImage(spacecraft3Img);
+    spacecraft.velocityX(5)
+   }
+
+   if(keyDown(39)){
+    spacecraft.addImage(spacecraft4Img);
+   }
+  }
+
+  drawSprites();
+}
